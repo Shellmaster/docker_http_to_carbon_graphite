6b300fc72c24284cf30663703d7f670c16ee703c
Shellmaster <rafal@maracje.pl>
2017-08-23 10:39:03 +1200

    First commit

---
+++ Python_Graphite.py
@@ -0,0 +1,86 @@
+#! /usr/local/bin/python
+
+import time
+import BaseHTTPServer
+import datetime
+import socket
+import os
+
+
+HOST_NAME = '0.0.0.0' # for docker it doesn't matter, bind to everything
+PORT_NUMBER = 12006 # inside the container so doesn't really matter, listen on 12006
+
+# Graphite settings -- IP and PORT to connect to graphite
+
+# set default server if not specified
+try:
+	SERVER = os.environ['GRAPHITE_PORT_2003_TCP_ADDR']
+except Exception:
+	print "Environment Variable GRAPHITE_PORT_2003_TCP_ADDR is not set , using port 2003"
+	SERVER = "127.0.0.1"
+
+# set default port if not specified
+try:
+	PORT = os.environ['GRAPHITE_IP_ADDR']
+except Exception:
+	print "Environment Variable GRAPHITE_IP_ADDR is not set , using 127.0.0.1"
+	PORT = 2003
+# enable debug mode
+try:
+	DEBUG = os.environ['DEBUG']
+except Exception:
+	print "Environment Variable DEBUG is not set DEBUG mode is disabled, to enable it set DEBUG=1"
+	DEBUG=0
+
+# set the connection details for graphite
+ADDR = (SERVER, PORT)
+
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.send_header("Wisdom", "Don't be naughty")
+        s.end_headers()
+    def do_GET(s):
+	timestamp = str( datetime.datetime.now().strftime("%s") )
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.send_header("X-Issues-Contact", "rafal@maracje.pl")
+        s.end_headers()
+
+	URI=s.path.split("/")
+	# get the value
+	VALUE=URI[-1]
+	# remove it from the array
+	URI.pop(-1)
+	# join an array with a spacebar then replace spacebar with a dot and remove first character
+	PARAMS=' '.join(URI).replace(" " ,".")[1:]
+        s.wfile.write("Sending as: ")
+	TO_SEND = PARAMS + " " + VALUE + " " + timestamp + "\n"
+        s.wfile.write(TO_SEND)
+	try:
+		ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		ss.connect(ADDR)
+		ss.send(TO_SEND)
+	except Exception:
+		pass
+	ss.close()
+    def log_message(self, format, *args):
+	if DEBUG:
+		print "Access log:", format % args
+	else:
+		return
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    print "Connecting to Graphite: %s:%s" % (SERVER, PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # http_to_carbon_graphite
 A simple translation service from http requests to graphite (carbon)
+
+At the current state there is no easy way to limit access to carbon if you are using proxy and proxy again etc.
+This is a simple idea of a web server which translates http requests and sends them to carbon through http
+so you can use nginx or apache and rewrite it with some IP restricions or even header/hashes.
+
+I am using a lot of custom scripts behind a proxy and doing reverse proxy to port 2003 is not ideal when you need to close all network connections
+and deal with re-connection etc. This is a very simple approach that I just need thus I am sharing it for someone with a simillar problem.
+
+
+Usage example:
+
+Don't detach:
+
+`docker run -ti --rm -e "DEBUG=1" -e "GRAPHITE_PORT_2003_TCP_ADDR=2003" -e "GRAPHITE_IP_ADDR=127.0.0.1" -p 127.0.0.1:12006:12006 shellmaster/http_to_carbon_graphite`
+
+Daemon mode: 
+
+When you have locally a proxy server in front of it:
+
+`docker run -d -e "GRAPHITE_PORT_2003_TCP_ADDR=2003" -e "GRAPHITE_IP_ADDR=127.0.0.1" -p 127.0.0.1:12006:12006 shellmaster/http_to_carbon_graphite`
+
+Access to everyone:
+
+`docker run -d -e "GRAPHITE_PORT_2003_TCP_ADDR=2003" -e "GRAPHITE_IP_ADDR=127.0.0.1" -p 12006:12006 shellmaster/http_to_carbon_graphite`
+
+
+The web server inside the container is listening on port 12006 you can use whatever you wish :)
+
+
+Optional Variables to Use:
+
+`GRAPHITE_PORT_2003_TCP_ADDR` -- port that carbon is listening on
+`GRAPHITE_IP_ADDR` -- carbon IP address
+`DEBUG=1` -- will show all access logs (no ip or date, it's only for debug mode) -- it will be super spammy, I am sending more than 10 000 metrics per 10 seconds
+
+
+
+And how to send some data to it:
+
+`curl -s http://127.0.0.1:12006/rafal-pc1/varnish/objects/active/1453232 > /dev/null`
+
+if you want to see what is being sent:
+
+`curl -i http://127.0.0.1:12006/rafal-pc1/varnish/objects/active/1453232`
+
+Or (mostly for Windows users) just open your browser and go to:
+
+`http://127.0.0.1:12006/rafal-pc1/varnish/objects/active/1453232`
+
+
+Good luck!
